76430e1a727ee703d617d94bea36b994a3ea6e8a
WinnerWind <71366717+WinnerWind@users.noreply.github.com>
2025-08-11 22:27:45 +0530

    Add a clock subtitle

--- clock.js
+++ clock.js
@@ -1,17 +1,25 @@
 const clock = document.querySelector(".clock")
+const subClock = document.querySelector(".clockSubtitle")
 
 SetTime()
 
 async function SetTime() {
 	const date = new Date()
 	const year = date.getFullYear()
+	const day = date.getDate()
+	const dayOfTheWeek = date.getDay()
+	const dayOfTheWeekName = new Intl.DateTimeFormat("en-US", {weekday: "long"}).format(date.dayOfTheWeek);
 	const month = date.getMonth()
+	const monthName = new Intl.DateTimeFormat("en-US", {month: "long"}).format(date.dayOfTheWeek);
+	const monthNameShort = new Intl.DateTimeFormat("en-US", {month: "short"}).format(date.dayOfTheWeek);
 	const hour = String(date.getHours()).padStart(2, '0')
 	const minutes = String(date.getMinutes()).padStart(2, '0')
 	const seconds = String(date.getSeconds()).padStart(2, '0')
 	const milliseconds = String(date.getMilliseconds()).padStart(3, '0')
-	const outputDate = `${hour}:${minutes}:${seconds}::${milliseconds}`
-	clock.innerText = outputDate
+
+
+	clock.innerText = `${hour}:${minutes}:${seconds}::${milliseconds}`
+	subClock.innerText = `${dayOfTheWeekName}, ${day} ${monthName} ${year}`
 
 	setTimeout(SetTime, 25)
 }

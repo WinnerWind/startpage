d60451dccfce5782bcc1021688de129a97f5a55c
WinnerWind <71366717+WinnerWind@users.noreply.github.com>
2025-08-12 12:58:05 +0530

    Change cursor size when hovering over element
    assumes that the entire thing loads in 100 milliseconds.

--- cursorGlow.js
+++ cursorGlow.js
@@ -1,9 +1,24 @@
 const glow = document.getElementById('cursorGlow');
-
 glow.style.transform = `translate(${window.innerWidth / 2}px, ${window.innerHeight / 2}px)`;
 
 window.addEventListener('mousemove', (e) => {
     const { clientX, clientY } = e;
     const glowSize = glow.offsetWidth;
     glow.style.transform = `translate(${clientX - glowSize / 2}px, ${clientY - glowSize / 2}px)`;
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(() => {
+        var allHoverElements = Array.from(document.querySelectorAll('a, input, button'))
+
+        allHoverElements.forEach(el => {
+            el.addEventListener('mouseover', (e) => {
+                glow.classList.add("elementHover")
+            });
+
+            el.addEventListener('mouseout', (e) => {
+                glow.classList.remove("elementHover")
+            });
+        })
+    }, 100)
 });
